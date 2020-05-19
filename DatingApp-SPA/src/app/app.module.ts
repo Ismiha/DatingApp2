99abc3f29dc1e00d4283c7577c6650c7c0a05654
Ismiha Ismail <Ismiha1975@gmail.com>
2020-05-19 11:58:37 +0800

    Add the Parent and Child connection

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -4,15 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+
 import { from } from 'rxjs';
 import { NavComponent } from './nav/nav.component';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent,
-      NavComponent
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
@@ -20,7 +23,9 @@ import { AuthService } from './_services/auth.service';
       HttpClientModule,
       FormsModule
    ],
-   providers: [AuthService],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,18 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-value',
-  templateUrl: './value.component.html',
-  styleUrls: ['./value.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class ValueComponent implements OnInit {
-    values: any;
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any = {};
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.GetValues();
   }
-
+  registerToggle(){
+    this.registerMode = true;
+  }
   GetValues(){
     this.http.get('http://localhost:5000/api/values').subscribe(response => {
       this.values = response;
@@ -21,5 +24,7 @@ export class ValueComponent implements OnInit {
       console.log(error);
     });
   }
-
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode = registerMode;
+  }
 }
